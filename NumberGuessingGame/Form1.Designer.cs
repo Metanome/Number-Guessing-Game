50a704721536b6289bbee8528beec9187efd42a5
Metanome <metanome12911@gmail.com>
2024-09-04 10:20:42 +0300

    Added project files via upload

---
+++ NumberGuessingGame/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace NumberGuessingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnCheck = new System.Windows.Forms.Button();
+            this.lbYou = new System.Windows.Forms.ListBox();
+            this.lbPC = new System.Windows.Forms.ListBox();
+            this.lblYou = new System.Windows.Forms.Label();
+            this.lblPC = new System.Windows.Forms.Label();
+            this.lblGuessed = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblWelcomeUser = new System.Windows.Forms.Label();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBoxNumber
+            // 
+            this.txtBoxNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxNumber.Location = new System.Drawing.Point(257, 73);
+            this.txtBoxNumber.Name = "txtBoxNumber";
+            this.txtBoxNumber.Size = new System.Drawing.Size(85, 20);
+            this.txtBoxNumber.TabIndex = 0;
+            this.txtBoxNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Guess a number between (1 - 10)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(348, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "(Min: 1, Max: 10)";
+            // 
+            // BtnCheck
+            // 
+            this.BtnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCheck.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.BtnCheck.Location = new System.Drawing.Point(208, 144);
+            this.BtnCheck.Name = "BtnCheck";
+            this.BtnCheck.Size = new System.Drawing.Size(91, 39);
+            this.BtnCheck.TabIndex = 3;
+            this.BtnCheck.Text = "Check";
+            this.BtnCheck.UseVisualStyleBackColor = true;
+            this.BtnCheck.Click += new System.EventHandler(this.BtnCheck_Click);
+            // 
+            // lbYou
+            // 
+            this.lbYou.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbYou.FormattingEnabled = true;
+            this.lbYou.Location = new System.Drawing.Point(59, 124);
+            this.lbYou.Name = "lbYou";
+            this.lbYou.Size = new System.Drawing.Size(143, 121);
+            this.lbYou.TabIndex = 4;
+            // 
+            // lbPC
+            // 
+            this.lbPC.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPC.FormattingEnabled = true;
+            this.lbPC.Location = new System.Drawing.Point(305, 124);
+            this.lbPC.Name = "lbPC";
+            this.lbPC.Size = new System.Drawing.Size(146, 121);
+            this.lbPC.TabIndex = 5;
+            // 
+            // lblYou
+            // 
+            this.lblYou.AutoSize = true;
+            this.lblYou.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYou.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lblYou.Location = new System.Drawing.Point(114, 106);
+            this.lblYou.Name = "lblYou";
+            this.lblYou.Size = new System.Drawing.Size(31, 15);
+            this.lblYou.TabIndex = 6;
+            this.lblYou.Text = "You";
+            // 
+            // lblPC
+            // 
+            this.lblPC.AutoSize = true;
+            this.lblPC.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPC.ForeColor = System.Drawing.Color.Red;
+            this.lblPC.Location = new System.Drawing.Point(365, 106);
+            this.lblPC.Name = "lblPC";
+            this.lblPC.Size = new System.Drawing.Size(25, 15);
+            this.lblPC.TabIndex = 7;
+            this.lblPC.Text = "PC";
+            // 
+            // lblGuessed
+            // 
+            this.lblGuessed.AutoSize = true;
+            this.lblGuessed.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuessed.Location = new System.Drawing.Point(205, 259);
+            this.lblGuessed.Name = "lblGuessed";
+            this.lblGuessed.Size = new System.Drawing.Size(71, 13);
+            this.lblGuessed.TabIndex = 8;
+            this.lblGuessed.Text = "Guessed: 0";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(205, 282);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(55, 13);
+            this.lblScore.TabIndex = 9;
+            this.lblScore.Text = "Score: 0";
+            // 
+            // lblWelcomeUser
+            // 
+            this.lblWelcomeUser.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblWelcomeUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcomeUser.ForeColor = System.Drawing.Color.Blue;
+            this.lblWelcomeUser.Location = new System.Drawing.Point(0, 0);
+            this.lblWelcomeUser.Name = "lblWelcomeUser";
+            this.lblWelcomeUser.Size = new System.Drawing.Size(511, 70);
+            this.lblWelcomeUser.TabIndex = 10;
+            this.lblWelcomeUser.Text = "lblWelcomeUser";
+            this.lblWelcomeUser.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnReset.ForeColor = System.Drawing.Color.Red;
+            this.BtnReset.Location = new System.Drawing.Point(208, 189);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(91, 39);
+            this.BtnReset.TabIndex = 11;
+            this.BtnReset.Text = "Reset";
+            this.BtnReset.UseVisualStyleBackColor = true;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 329);
+            this.Controls.Add(this.BtnReset);
+            this.Controls.Add(this.lblWelcomeUser);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblGuessed);
+            this.Controls.Add(this.lblPC);
+            this.Controls.Add(this.lblYou);
+            this.Controls.Add(this.lbPC);
+            this.Controls.Add(this.lbYou);
+            this.Controls.Add(this.BtnCheck);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxNumber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnCheck;
+        private System.Windows.Forms.ListBox lbYou;
+        private System.Windows.Forms.ListBox lbPC;
+        private System.Windows.Forms.Label lblYou;
+        private System.Windows.Forms.Label lblPC;
+        private System.Windows.Forms.Label lblGuessed;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblWelcomeUser;
+        private System.Windows.Forms.Button BtnReset;
+    }
+}
+
---
+++ NumberGuessingGame/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberGuessingGame
+{
+    public partial class Form1 : Form
+    {
+        Random randomNumber = new Random();
+
+        int randnum = 0;
+        int guesses = 0;
+        int score = 0;
+
+        string userName = Environment.UserName;
+        public Form1()
+        {
+            InitializeComponent();
+            loadGenerator();
+            lblWelcomeUser.Text = "Welcome " + userName;
+        }
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            int input;
+            // Parse and convert the input string to an integer.
+            if (Int32.TryParse(txtBoxNumber.Text, out input))
+            {
+                // Add the parsed input and random number to list boxes, focus and clear the TextBox.
+                lbYou.Items.Add(txtBoxNumber.Text);
+                lbPC.Items.Add(randnum);
+                txtBoxNumber.Focus();
+                txtBoxNumber.Clear();
+
+                // If input is equal to the generated number; increment the score count, throw a success message, generate a new random number, clear and focus on TextBox and reset the guess count.
+                if (input == randnum)
+                {
+                    score += 10;
+                    lblScore.Text = "Score: " + score;
+                    guesses = 0;
+                    lblGuessed.Text = "Guessed: " + guesses;
+                    MessageBox.Show("Great, you guessed it right! Wanna try again?");
+                    loadGenerator();
+                    txtBoxNumber.Clear();
+                    txtBoxNumber.Focus();
+                }
+                // If input is less than or greater than the generated number; generate a new random number, increment guess count and reset the score count.
+                else if (input < randnum || input > randnum)
+                {
+                    loadGenerator();
+                    guesses += 1;
+                    lblGuessed.Text = "Guessed: " + guesses + " times";
+                    score = 0;
+                    lblScore.Text = "Score: " + score;
+                }
+
+                // When the input is greater than 10 or less than 1; Reset guess count, clear listbox items and throw an error.
+                if (input > 10 || input < 1)
+                {
+                    guesses = 0;
+                    lblGuessed.Text = "Guessed: " + guesses;
+                    lbYou.Items.Clear();
+                    lbPC.Items.Clear();
+                    MessageBox.Show("That's an invalid number.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            // Throw an error, focus on TextBox when there is no input from the user.
+            else if (txtBoxNumber.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("Please enter a number between (1 - 10) to proceed.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtBoxNumber.Focus();
+            }
+        }
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            // Reset guess count, score count; Clear listbox items; Clear and focus on TextBox.
+            guesses = 0;
+            lblGuessed.Text = "Guessed: " + guesses;
+            score = 0;
+            lblScore.Text = "Score: " + score;
+            lbYou.Items.Clear();
+            lbPC.Items.Clear();
+            txtBoxNumber.Clear();
+            txtBoxNumber.Focus();
+        }
+        private void loadGenerator()
+        {
+            // Generate a random number between (1 - 10)
+            randnum = randomNumber.Next(1, 10);
+        }
+    }
+}
